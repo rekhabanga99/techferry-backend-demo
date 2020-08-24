84a356161027579d99f3f3a15975f61c3864d52b
rekha <rekha@techferry.com>
2020-08-24 12:58:49 +0530

    Socet . io added

--- app.js
+++ app.js
@@ -68,6 +68,11 @@ mongoose
     'mongodb://localhost:27017/messages?authSource=admin'
   )
   .then(result => {
-    app.listen(8080);
+    
+    const server = app.listen(8080);
+    const io = require('socket.io')(server);
+    io.on('connection',socket =>{
+      console.log('Client connected')
+    })
   })
   .catch(err => console.log(err));
